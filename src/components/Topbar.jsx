a3a85985b8150829b0b98074267d710340af7ed4
N Jayant Rao <njayantrao@gmail.com>
2025-12-08 13:23:10 +0530

    Updated user profile page

--- src/components/Topbar.jsx
+++ src/components/Topbar.jsx
@@ -1,13 +1,23 @@
+import ImageAvatars from "./ImageAvatar"
 import LeftSection from "./LeftSection"
+import BackgroundLetterAvatars from "./TextAvatar"
 
 function Topbar(){
     return(
-        <div className="h-25 w-full bg-[#5B21B6] flex justify-between  items-center shadow-xl text-xl font-bold text-gray-300">
-            <LeftSection />
-            <div className="text-2xl font-bold text-[#d0d4db] hover:cursor-pointer relative dropdown">Civilian Dashboard</div>
+        <div className="h-15 w-full bg-[#1D4ED8] flex justify-between  items-center shadow-xl text-xl font-bold text-gray-300">
+            <div className="w-1/5 flex justify-center gap-1 items-center">
+                <img src="https://res.cloudinary.com/dpwqlb3d7/image/upload/v1764314433/My%20Brand/Gemini_Generated_Image_o5l8fro5l8fro5l8_s8srd9.png" alt="Nagrik setu" className="h-12 w-12 rounded-full"/>
+                <div className="hover:cursor-pointer relative dropdown text-lg">
+                <h1>Nagrik Setu</h1>
+            </div>
+            </div>
+            <div className="text-2xl font-bold text-[#d0d4db] hover:cursor-pointer relative dropdown">Dashboard</div>
             <div className="flex gap-5 mr-2 p-2 ">
-                <div className="hover:cursor-pointer relative dropdown"><h2>Help</h2></div>
-                <div className="hover:cursor-pointer relative dropdown"><h2>Contact Us</h2></div>
+                <div className="hover:cursor-pointer relative dropdown text-lg"><h2>Hi, Rahul Sharma</h2></div>
+                <div className="hover:cursor-pointer relative dropdown">
+                    <BackgroundLetterAvatars string={"Rahul Sharma"}/>
+                    {/* <ImageAvatars /> */}
+                </div>
             </div>
         </div>
     )
--- src/components/UserMain.jsx
+++ src/components/UserMain.jsx
@@ -1,8 +1,89 @@
+import ComplaintCard from "./ComplaintCard"
+import { FileText,FileClock,FileCheck,FileX } from "lucide-react"
+import Complaints from "./Complaints"
+
+
 function UserMain(){
+    const cardInfo= [
+        {
+            bgcolor:"bg-[#2480c6]",
+            text:"Filed",
+            count:0,
+            icon:FileText,
+            size:"text-2xl",
+        },
+         {
+            bgcolor:"bg-[#F9A825]", 
+            text:"In-Progress",
+            count:0,
+            icon:FileClock,
+            size:"text-xl",
+        },
+         {
+            bgcolor:"bg-[#8bc34a]",
+            text:"Resolved",
+            count:0,
+            icon:FileCheck,
+            size:"text-2xl",
+        },
+         {
+            bgcolor:"bg-[#f54336]",
+            text:"Rejected",
+            count:0,
+            icon:FileX,
+            size:"text-2xl",
+        },
+    ]
+
+    const recents= [
+        {
+            bgcolor:"bg-[#2480c6]",
+            text:"Filed",
+            count:0,
+            icon:FileText,
+            size:"text-2xl",
+        },
+         {
+            bgcolor:"bg-[#F9A825]", 
+            text:"In-Progress",
+            count:0,
+            icon:FileClock,
+            size:"text-xl",
+        },
+         {
+            bgcolor:"bg-[#8bc34a]",
+            text:"Resolved",
+            count:0,
+            icon:FileCheck,
+            size:"text-2xl",
+        },
+         {
+            bgcolor:"bg-[#f54336]",
+            text:"Rejected",
+            count:0,
+            icon:FileX,
+            size:"text-2xl",
+        },
+    ]
     return(
-        <div className="relative w-full h-screen">
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-2xl">
-                <h2>Hello</h2>
+        <div className="relative w-full h-screen p-4">
+            <div className="text-3xl text-[#1E3A8A] mb-2 font-bold px-2 tracking-tight">
+                <h1>Complaint Insights</h1>
+            </div>
+            <div className=" flex gap-4 justify-evenly flex-nowrap">
+                {
+                    cardInfo.map((ele,idx)=>{
+                        return <ComplaintCard key={idx} bgcolor={ele.bgcolor} text={ele.text} count={ele.count} icon={ele.icon} size={ele.size}/>
+                    })
+                }
+            </div>
+            <div className="text-3xl text-[#1E3A8A] mt-5 mb-3 font-bold px-2 tracking-tight">
+                <h1>Recent Complaint </h1>
+            </div>
+            <div className=" flex gap-2 justify-evenly flex-col">
+                <Complaints />
+                <Complaints />
+                <Complaints />
             </div>
         </div>
     )
